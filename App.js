74e96a1d1ab3a163d03d1c29815f116a7690a956
victorcheng7 <victor_cheng@yahoo.com>
2019-03-16 00:01:26 -0700

    react native initialization

--- App.js
+++ App.js
@@ -4,10 +4,36 @@ import { AppLoading, Asset, Font, Icon } from 'expo';
 import AppNavigator from './navigation/AppNavigator';
 import { Ionicons } from '@expo/vector-icons';
 
+//import firebase from 'expo-firebase-app';
+import * as firebase from 'firebase';
+
+import 'firebase/auth';
+import 'firebase/database';
+
+const config = {
+  apiKey: 'AIzaSyCFFGwFNvO1_fHHLXjzXW8e7nxx7HLVuk0',
+  authDomain: 'taskup-8a36a.firebaseapp.com',
+  databaseURL: 'https://taskup-8a36a.firebaseio.com',
+  projectId: 'taskup-8a36a',
+  storageBucket: 'taskup-8a36a.appspot.com',
+  messagingSenderId: '81221159592',
+};
+
+
 export default class App extends React.Component {
-  state = {
-    isLoadingComplete: false,
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+        isLoadingComplete: false,
+    };
+
+    // Initialize firebase
+    if(!firebase.apps.length) { firebase.initializeApp(config); }
+    firebase.database().ref('/categories/Driving/ITUedQpWeESsaQObg9gUNsNu0vj2').once('value').then(function(snapshot) {
+      console.log(snapshot);
+    });
+  }
+
 
   async componentDidMount() {
     await Font.loadAsync({
---
+++ constants/ApiKeys.js
@@ -0,0 +1,10 @@
+export default {
+	FirebaseConfig: {
+	  apiKey: 'AIzaSyCFFGwFNvO1_fHHLXjzXW8e7nxx7HLVuk0',
+	  authDomain: 'taskup-8a36a.firebaseapp.com',
+	  databaseURL: 'https://taskup-8a36a.firebaseio.com',
+	  projectId: 'taskup-8a36a',
+	  storageBucket: 'taskup-8a36a.appspot.com',
+	  messagingSenderId: '81221159592'
+	},
+}

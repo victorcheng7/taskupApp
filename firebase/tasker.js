f2076c6c882f571bd7e3f4de06faed6e24c615a4
meskedal <mariuesk@stud.ntnu.no>
2019-03-18 02:11:27 -0700

    [Feature]

--- firebase/tasker.js
+++ firebase/tasker.js
@@ -18,12 +18,12 @@ export const getTasker = (uid, callback) => {
 };
 
 export const setAvailability = (categoryID, subCategoryID, available, uid) => {
-  firebase.database().ref(`taskers/${uid}/categories/${categoryID}/${subCategoryID}/available`).once('value').set(available);
+  firebase.database().ref(`taskers/${uid}/categories/${categoryID}/${subCategoryID}/available`).set(available);
 };
 
 export const setAvailabilities = (categories, available, uid) => {
-  Object.keys(categories).forEach((categoryID) => {
-    Object.keys(categories[categoryID]).forEach((subCategoryID) => {
+  Object.keys(categories.category_ids).forEach((categoryID) => {
+    Object.keys(categories[categoryID].sub_category_ids).forEach((subCategoryID) => {
       setAvailability(categoryID, subCategoryID, available, uid);
     });
   });
